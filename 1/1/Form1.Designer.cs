befd373fcd32e48f025dad9634267d6618d84e76
alex-301223 <alex_petrov_301223>
2019-11-07 15:16:40 +0300

    07.11.2019

---
+++ 1/1/Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace _1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picHisto = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picHisto)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picHisto
+            // 
+            this.picHisto.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.picHisto.Location = new System.Drawing.Point(0, 0);
+            this.picHisto.Name = "picHisto";
+            this.picHisto.Size = new System.Drawing.Size(800, 450);
+            this.picHisto.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picHisto.TabIndex = 0;
+            this.picHisto.TabStop = false;
+            this.picHisto.Paint += new System.Windows.Forms.PaintEventHandler(this.PicHisto_Paint);
+            this.picHisto.Resize += new System.EventHandler(this.PicHisto_Resize);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.picHisto);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.picHisto)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picHisto;
+    }
+}
---
+++ 1/1/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public int[] val;
+        public bool flag = false;
+        private void PicHisto_Resize(object sender, EventArgs e)
+        {
+            picHisto.Refresh();
+
+        }
+
+        private void PicHisto_Paint(object sender, PaintEventArgs e)
+        {
+            DrawHistogram(e.Graphics, picHisto.BackColor, val,
+        picHisto.ClientSize.Width, picHisto.ClientSize.Height);
+        }
+
+
+
+
+
+        private void DrawHistogram(Graphics gr, Color back_color,
+        int[] values, int width, int height)
+        {
+            Color[] Colors = new Color[] {
+Color.Red, Color.LightGreen, Color.Blue,
+Color.Pink, Color.Green, Color.LightBlue,
+Color.Orange, Color.Yellow, Color.Purple
+};
+
+            gr.Clear(back_color);
+
+            // Сделаem преобразование в PictureBox. 
+            RectangleF data_bounds =
+            new RectangleF(0, 0, values.Length, 200);
+            PointF[] points =
+            {
+new PointF(0, height),
+new PointF(width, height),
+new PointF(0, 0)
+};
+            Matrix transformation = new Matrix(data_bounds, points);
+            gr.Transform = transformation;
+
+            // Нарисуем гистограмму. 
+            for (int i = 0; i < values.Length; i++)
+                using (Pen thin_pen = new Pen(Colors[i % Colors.Length], 0))
+                {
+
+                    {
+
+
+                        RectangleF rect = new RectangleF(i, 0, 1, values[i]);
+                        using (Brush the_brush =
+                        new SolidBrush(Colors[i % Colors.Length]))
+
+
+                        {
+                            gr.FillRectangle(the_brush, rect);
+                            gr.DrawRectangle(thin_pen, rect.X, rect.Y,
+                            rect.Width, rect.Height);
+
+                        }
+
+
+
+                    }
+                }
+
+
+
+            gr.ResetTransform();
+            gr.DrawRectangle(Pens.Black, 0, 0, width - 1, height - 1);
+        }
+    }
+    
+}
+
